8fe721e369dc71ba0bb25674e40f4752b081da22
Vinícius <me@vkhashimoto.dev>
2022-08-21 17:39:10 -0300

    Add which-key config, mappings and helper

---
+++ .config/nvim/lua/plugins/config/which-key.lua
@@ -0,0 +1,37 @@
+local wk = require("which-key")
+
+wk.setup({
+	plugins = {
+		marks = false,
+		presets = {
+			operators = true,
+			motions = true,
+			text_objects = true,
+		},
+	},
+	operators = { gc = "Comments" },
+	window = {
+		border = "single",
+		position = "bottom",
+		margin = { 1, 0, 1, 0 },
+		padding = { 2, 2, 2, 2 },
+	},
+	layout = {
+		height = { min = 4, max = 25 },
+		width = { min = 20, max = 50 },
+		spacing = 2,
+		align = "center",
+	}
+})
+
+-- register mappings with <leader>
+wk.register({
+	f = {
+		name = "find",
+		f = { "<cmd>Telescope find_files<cr>", "Find file" },
+		s = { "<cmd>Telescope live_grep<cr>", "Search string" },
+		w = { "<cmd>Telescope grep_string<cr>", "Search current string" },
+		m = { "<cmd>Telescope marks<cr>", "Find marks" },
+	}
+
+}, { prefix = "<leader>"} )
