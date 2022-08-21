8f0c9f56f90edfc781dd0df576eb741ea873ff45
Vinícius <me@vkhashimoto.dev>
2022-08-21 17:38:13 -0300

    Sets up some basic config for telescope

--- .config/nvim/lua/plugins/config/telescope.lua
+++ .config/nvim/lua/plugins/config/telescope.lua
@@ -1,7 +1,38 @@
 local telescope = require("telescope")
+local actions = require("telescope.actions")
+local action_layout = require("telescope.actions.layout")
 
 telescope.setup({
-	sorting_strategy = "descending",
-	layout_strategy = "horizontal"
+	defaults = {
+		file_ignore_patterns = {
+			".git"
+		},
+		prompt_prefix = "-> ",
+		selection_caret = "#> ",
+		path_display = { "smart" },
+		mappings = {
+			i = {
+				["jk"] = actions.close,
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-p>"] = action_layout.toggle_preview,
+				-- show key mappings
+				-- ["<C-?>"] = actions.which_key
+			}
+		}
+	--	layout_config = {
+	--		vertical = { width = 0.1 }
+	--	}
+	},
+	pickers = {
+		find_files = {
+			hidden = true
+		},
+		live_grep = {
+			additional_args = function (opts)
+				-- search on hidden files
+				return {"--hidden"}
+			end
+		}
+	}
 })
-
