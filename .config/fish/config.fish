77e7c87c835ce009516d3279beefe9949c021ef0
vkhashimoto <me@vkhashimoto.dev>
2022-11-02 22:06:50 -0300

    change env var and add emacs daemon

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,15 @@
+set -g ALTERNATE_EDITOR ""
+set -g EDITOR "emacsclient -t -a ''"
+set -g VISUAL "emacsclient -c -a emacs"
+. /opt/asdf-vm/asdf.fish
+. ~/.asdf/plugins/java/set-java-home.fish
+
 if status is-interactive
    # Commands to run in interactive sessions can go here
-   set -g ALTERNATE_EDITOR ""
-   set -g EDITOR "emacsclient -t -a ''"
-   set -g VISUAL "emacsclient -c -a emacs"
    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+      /usr/bin/emacs --fg-daemon --eval '(load-file "~/.emacs.d/init.el")'
       exec startx
    end
-   . /opt/asdf-vm/asdf.fish
-   . ~/.asdf/plugins/java/set-java-home.fish
 
 end
 
