b5814be4d57f5c4ceb3f6b19a24748509f19d1e3
Vinícius Hashimoto <me@vkhashimoto.dev>
2023-07-09 03:29:07 -0300

    change line number colors

--- .config/nvim/lua/plugins/ui/colorscheme/config/catppuccin.lua
+++ .config/nvim/lua/plugins/ui/colorscheme/config/catppuccin.lua
@@ -1,3 +1,15 @@
+local frappe = require("catppuccin.palettes").get_palette "frappe"
 require("catppuccin").setup {
-	flavour = "macchiato"
+	flavour = "macchiato",
+	highlight_overrides = {
+		macchiato = function(--[[ macchiato ]])
+			return {
+				LineNr = { fg = frappe.red },
+				--CursorLineNr = { fg = macchiato.red },
+				LineNrAbove = { fg = frappe.lavender },
+				LineNrBelow = { fg = frappe.teal },
+			}
+		end,
+
+	}
 }
