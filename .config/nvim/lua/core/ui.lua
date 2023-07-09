df9a8ed1bb9712621999ef8d966d977119c81d4e
Vinícius Hashimoto <me@vkhashimoto.dev>
2023-07-09 18:42:46 -0300

    change ui settings/colors

--- .config/nvim/lua/core/ui.lua
+++ .config/nvim/lua/core/ui.lua
@@ -1,2 +1,10 @@
 vim.wo.number = true
 vim.wo.relativenumber = true
+
+
+vim.opt.hlsearch = false
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.cursorline = true
--- .config/nvim/lua/plugins/ui/colorscheme/config/catppuccin.lua
+++ .config/nvim/lua/plugins/ui/colorscheme/config/catppuccin.lua
@@ -2,10 +2,11 @@ local frappe = require("catppuccin.palettes").get_palette "frappe"
 require("catppuccin").setup {
 	flavour = "macchiato",
 	highlight_overrides = {
-		macchiato = function(--[[ macchiato ]])
+		macchiato = function(macchiato)
 			return {
 				LineNr = { fg = frappe.red },
-				--CursorLineNr = { fg = macchiato.red },
+				CursorLine = { underline = true, sp = macchiato.pink },
+				CursorLineNr = { fg = frappe.red, underline = true },
 				LineNrAbove = { fg = frappe.lavender },
 				LineNrBelow = { fg = frappe.teal },
 			}
