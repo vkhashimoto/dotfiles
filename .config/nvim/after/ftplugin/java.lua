b221f0c3bd9679b80551c7d3a0e7fc9ecde2fa31
Vinícius Hashimoto <me@vkhashimoto.dev>
2023-07-10 13:20:54 -0300

    fix jdtls workspace

--- .config/nvim/after/ftplugin/java.lua
+++ .config/nvim/after/ftplugin/java.lua
@@ -44,6 +44,12 @@ ls.add_snippets("java", {
 
 local lspconfig = require("lspconfig")
 local lsp_defaults = lspconfig.util.default_config
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+--local workspace_dir = '/home/vkhashimoto/nvim-jdtls-workspace' .. project_name
+
+local root_markers = { "gradlew", "mvnw", ".git", "pom.xml", "build.gradle" }
+local root_dir = require("jdtls.setup").find_root(root_markers)
+local workspace_folder = os.getenv("HOME") .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
 
 local capabilities = vim.tbl_deep_extend(
 	"force",
@@ -51,9 +57,9 @@ local capabilities = vim.tbl_deep_extend(
 	require("cmp_nvim_lsp").default_capabilities()
 )
 
-lspconfig.jdtls.setup {
-	capabilities = capabilities,
-}
+--lspconfig.jdtls.setup {
+--	capabilities = capabilities,
+--}
 
 vim.cmd "LspStart"
 
@@ -64,11 +70,14 @@ local config = {
 				settings = {
 					url = "<FORMATTER_FILE_PATH>"
 				}
-			}
+			},
 		}
 	},
-	cmd = { "jdtls" },
-	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw"}, { upward = true })[1]),
+	cmd = { 
+		"jdtls",
+		"-data", workspace_folder
+	},
+	root_dir = root_dir,
 }
 
 if config["settings"]["java"]["format"]["settings"]["url"] == "<FORMATTER_FILE_PATH>" then
@@ -79,3 +88,5 @@ end
 
 
 require("jdtls").start_or_attach(config)
+
+
