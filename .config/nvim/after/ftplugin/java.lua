dfbb382e65cf50eef739417f7d25c7e1c5acdcf3
Vinícius Hashimoto <me@vkhashimoto.dev>
2023-07-09 22:04:16 -0300

    add java snippets

--- .config/nvim/after/ftplugin/java.lua
+++ .config/nvim/after/ftplugin/java.lua
@@ -1,3 +1,47 @@
+-- snippets TODO: Move to another file?
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+
+ls.add_snippets("java", {
+	s("main", {
+		t({ "public static void main(String args[]) {" }),
+		t({ "", "	" }), i(0),
+		t({ "", "}" }),
+	}),
+	s("mainp", {
+		t({ "public static void main(String args[]) {" }),
+		t({ "", [[	System.out.println("]] }), i(0),
+		t({ [[");]] }),
+		t({ "", "}" }),
+
+	}),
+	s("mainf", {
+		t({ "public static void main(String args[]) {" }),
+		t({ "", "	System.out.println(" }), i(0),
+		t({ ");" }),
+		t({ "", "}" }),
+	});
+	s("print", {
+		t({ [[System.out.println("]] }), i(0),
+		t({ [[");]] }),
+	}),
+	s("printv", {
+		t({ "System.out.println(" }), i(0),
+		t({ ");" }),
+	}),
+	s("today", {
+		t({ "LocalDate today = LocalDate.today();" }),
+		t({ "", "" }), i(0),
+	}),
+	s("now", {
+		t({ "LocalDateTime now = LocalDateTime.now();" }),
+		t({ "", "" }), i(0),
+	})
+})
+
+
 local lspconfig = require("lspconfig")
 local lsp_defaults = lspconfig.util.default_config
 
