49bd0e79618bb9a621b2b6c7ad0950dadadca58d
Vinícius Hashimoto <me@vkhashimoto.dev>
2023-07-10 23:30:34 -0300

    fix jdtls

--- .config/nvim/after/ftplugin/java.lua
+++ .config/nvim/after/ftplugin/java.lua
@@ -45,11 +45,10 @@ ls.add_snippets("java", {
 local lspconfig = require("lspconfig")
 local lsp_defaults = lspconfig.util.default_config
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
---local workspace_dir = '/home/vkhashimoto/nvim-jdtls-workspace' .. project_name
 
 local root_markers = { "gradlew", "mvnw", ".git", "pom.xml", "build.gradle" }
 local root_dir = require("jdtls.setup").find_root(root_markers)
-local workspace_folder = os.getenv("HOME") .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+local workspace_folder = os.getenv("HOME") .. "/.local/share/jdtls-workspace/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
 
 local capabilities = vim.tbl_deep_extend(
 	"force",
@@ -57,26 +56,33 @@ local capabilities = vim.tbl_deep_extend(
 	require("cmp_nvim_lsp").default_capabilities()
 )
 
---lspconfig.jdtls.setup {
---	capabilities = capabilities,
---}
 
-vim.cmd "LspStart"
-
+local settings = {
+	java = {
+		format = {
+			settings = {
+				url = "<FORMATTER_FILE_PATH>"
+			}
+		},
+	}
+}
 local config = {
-	settings = {
-		java = {
-			format = {
-				settings = {
-					url = "<FORMATTER_FILE_PATH>"
-				}
-			},
-		}
-	},
 	cmd = { 
-		"jdtls",
-		"-data", workspace_folder
+	    'java',
+	    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+	    '-Dosgi.bundles.defaultStartLevel=4',
+	    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+	    '-Dlog.protocol=true',
+	    '-Dlog.level=ALL',
+	    '-Xmx1g',
+	    '--add-modules=ALL-SYSTEM',
+	    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+	    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+	    '-jar', '<JDTLS_ECLIPSE_EQUINOX_LAUNCHER>',
+	    '-configuration', '<JDTLS_CONFIG_FOLDER>',
+	    '-data', workspace_folder,
 	},
+	settings = settings,
 	root_dir = root_dir,
 }
 
@@ -86,6 +92,27 @@ if config["settings"]["java"]["format"]["settings"]["url"] == "<FORMATTER_FILE_P
 	})
 end
 
+--TODO: Create a method to notify error and group by "notification_type"
+local error = false
+for key, value in pairs(config.cmd) do
+	if value == "<JDTLS_ECLIPSE_EQUINOX_LAUNCHER>" then
+		vim.notify("jdtls eclipse equinox launcher not set", vim.log.levels.ERROR, {
+			title = "Java DE"
+		})
+		error = true
+	end
+
+	if value == "<JDTLS_CONFIG_FOLDER>" then
+		vim.notify("jdtls config folder not set", vim.log.levels.ERROR, {
+			title = "Java DE"
+		})
+	error = true
+	end
+end
+
+if error then
+	return
+end
 
 require("jdtls").start_or_attach(config)
 
