d6b3eaa4bdeb5859c7437e5670d46dcee93c0548
Vinícius Hashimoto <me@vkhashimoto.dev>
2023-07-13 07:47:31 -0300

    add telescope-ui-select

--- .config/nvim/lua/plugins/telescope/init.lua
+++ .config/nvim/lua/plugins/telescope/init.lua
@@ -1,7 +1,15 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.2',
-      	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function() 
-		require("plugins.telescope.config.keymap")
-	end,
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.2',
+      		dependencies = { 'nvim-lua/plenary.nvim' },
+		config = function() 
+			require("plugins.telescope.config.keymap")
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("plugins.telescope.config.telescope-ui-select")
+		end,
+	}
 }
