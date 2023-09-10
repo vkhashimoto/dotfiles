8980e360b71987368974cc475d992a18bbada4d3
Vinícius Hashimoto <me@vkhashimoto.dev>
2023-09-10 20:16:31 -0300

    change clipboard config

--- .config/nvim/lua/core/keymap/init.lua
+++ .config/nvim/lua/core/keymap/init.lua
@@ -4,5 +4,8 @@ local map = require("core.keymap.utils").map
 -- define <leader>
 vim.g.mapleader = " "
 
+-- share system clipboard
+vim.opt.clipboard = "unnamedplus"
+
 map("i", "jk", "<ESC>")
 map("i", "<ESC>", "<nop>")
